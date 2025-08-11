274ed186c810ce27a57ae95c1a488f70441cd320
derekHao <derek89757@gmail.com>
2025-08-11 20:49:45 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import { resolve } from "path";
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss(),tanstackRouter({
+    target: 'react',
+    autoCodeSplitting: true,
+  })],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    }
+  },
 })
